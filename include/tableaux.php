73b8d277d11a67684a86e3c4775e140813119ef3
Stelian-Constantin Stanciu <stanciu.stelian86@yahoo.fr>
2023-02-10 16:49:03 +0100

    Fonction interne de PHP

--- include/tableaux.php
+++ include/tableaux.php
@@ -5,44 +5,56 @@ include './fonctions.php';
 //Tableau indexé
 //declaration d'un tableau 
 
- $tableau = [];
- $tableau2 = array();
+//  $tableau = [];
+//  $tableau2 = array();
 
- $tableau3 = [
-     true,
-     4,
-    6.7,     [
-         5, 
-         'truc',
-        [
-            'muche',
-             8,
-             8.9
-         ]
-     ]
-];
- dd($tableau3);
 
-$tableau=[];
+//  dd($tableau3);
+
+// $tableau=[];
 // $tableau='Janvier';
 // $tableau=45; //ca remplace les valeurs precedents
 
 //pour r'ajouter des valeurs
-array_push($tableau,'Voiture',true);
+// $tableau = [
+//     true,
+//     4,
+//     6.7,     [
+//         5,
+//         'truc',
+//         [
+//             'muche',
+//             8,
+//             8.9
+//         ]
+//     ]
+// ];
+// $notes = [12, 9, 13, 7, 2];
+// array_push($tableau, 'Voiture', true);
 //dd($tableau); -Afficher le tableau
 //Afficher les nr des elements dans le tableau
-$notes = [12,9,13,7,2];
-dd($notes);
+
+// dd($notes);
 //fusioner des tableau
-$tableauFusion=array_merge($tableau,$notes);
-dd($tableauFusion);
+// $tableauFusion = array_merge($tableau, $notes);
+// dd($tableauFusion);
 //Afficher la note 13 dans le tableau notes
 //Afficher la note 13 dans tableau tableauFusion
-dd($notes[2]);
-
-
-
-
-
-
-
+// dd($notes[2]);
+// dd($tableauFusion[6]);
+//Afficher le chiffre 5 dans le tableau 
+//tableauFusion
+// dd($tableauFusion[3][0]);
+// dd($tableauFusion[3][2][2]);
+// $fruit = [
+//     'banane' => 'jaune',
+//     'pomme' => 'rouge',
+//     'kiwi' => 'vert'
+// ];
+// // dd($fruit['kiwi']);
+// $fruits['poire'] = 'rouge';
+// $chaine = implode('-', $fruits);
+// //dd($chaine);
+// $fruits2 = explode('*', $chaine);
+// dd($fruits2);
+//fonction associes au tableau
