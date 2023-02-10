c99f5baea2b388a4952f6e46bbf508b130c7081e
Stelian-Constantin Stanciu <stanciu.stelian86@yahoo.fr>
2023-02-10 14:38:57 +0100

    WORK

---
+++ include/fonctions.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+//function calcul($prixHT, $tva)
+//{
+//  $prixHT = 34;
+//$tva = 10;
+//$prixTTC = $prixHT + ($prixHT * $tva) / 100;
+//return $prixTTC;
+//}
+//echo calcul(34,10);
+
+//calcul TTC
+//fonction
+function calculPrixTTC($prixht, $tva)
+{
+
+    $prixTTC = $prixht + ($prixht * $tva) / 100;
+    return $prixTTC;
+}
+// echo calculPrixTTC(34, 10);
+//calcul age
+function calculAge($anneDeNaissance)
+{
+    $anneeEnCours = date("Y");
+    $age = $anneeEnCours - $anneDeNaissance;
+    return $age;
+}
+
+
+//function calcul($a, $b = 5) $b- constante
+//{
+// $c = $a + $b;
+//return $c;
+
+//}
+//echo calcul(23);
+
+// define('xxx', 10);
+// echo 'La valeur de xxx est de '.xxx;
+//function calcul($anneeDeNaissance, $anneeEnCours)
+//{
+//  $anneeDeNaissance = 1986;
+//$anneeEnCours = date("Y");
+//$age = $anneeEnCours - $anneeDeNaissance;
+
+// return $age;
+//}
+//echo calcul(1986, "Y");
+
+
+//fonction dd
+function dbug($valeur)
+{
+    echo "<pre>";
+    var_dump($valeur);
+    echo "</pre>";
+}
+
+function dd($valeur)
+{
+    echo "<pre style='background-color:green;overflow: auto;height: 300px;color:white;'>";
+    var_dump($valeur);
+    echo "</pre>";
+    die();
+}
+//on forces le typage : int
+// function maFonction(): int
+// {
+//     return 3;
+// }
+// dd(maFonction());
+
+//function qui n'envoye rien !!
+//  function maFonction(): void
+// {
+//      return null;
+//  }
+//  dd(maFonction());
+
+//fonction pour envoyer un tableau ou int ou float
+//function maFonction(): array|int|float
+//pour envoyer toute les types
+// function maFonction(): mixed
+// {
+//     return 4.5;
+// }
+// dd(maFonction());
+
+// function maFonction(array|int $x): mixed
+// {
+//     return $x;
+// }
+// dd(maFonction(34));
+
+//inverser une chaine de caractere
+$article="Je suis un article";
+// echo strrev($article);
+//Transforme une chaine de caractere en majuscule
+// echo strtoupper($article);
+//Longueur d'une chaine de caractere
+// echo strlen($article);
+//Remplace un element dans une chaine de caractere
+//replacer 'un' par 'cet'
+// echo  str_replace("un","cet", $article);
+//Retourne un segment d'une chaine de caractere
+//renvoyer les 6premieres elements d'une chaine de caracteres
+echo substr($article, 0, 7);
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+include './partials/header.php'; //si il n'existe pas il execute
+//la page qd meme 
+//require './partials/header.php'; // si il n'existe pas n'execute pas la page
+//include_once './partials/header.php ; //
+include './include/fonctions.php';
+
+?>
+<? dd($_SERVER)?>
+<p>Vous avez <? calculAge(1986); ?> ans</p>
+<header>
+    <?php $titre = 'Cours PHP'; ?>
+    <h1><?= $titre ?></h1>
+    <?php include './partials/menu.php'; ?>
+</header>
+
+<main>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Molestiae maxime officiis architecto, deserunt quas suscipit modi
+            ad qui totam ratione reprehenderit voluptatem aliquam corrupti sunt
+            adipisci alias nulla numquam accusantium.</p>
+    </section>
+</main>
+
+
+<?php
+
+include './partials/footer.php';
+?>
